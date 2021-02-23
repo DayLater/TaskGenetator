800eeec7c9ce24575c9f2d998ed202ae69eaca41
Daulet Basharov <d.basharov@kefirgames.com>
2021-02-23 18:27:52 +0300

    add: test union

--- ConsoleTaskGenerator/Program.cs
+++ ConsoleTaskGenerator/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ConsoleTaskGenerator
+﻿using System;
+using System.Linq;
+
+namespace ConsoleTaskGenerator
 {
     internal static class Program
     {
         public static void Main(string[] args)
         {
+            var firstArray = new[] {1, 2, 3, 4};
+            var secondArray = new[] {2, 5};
+            var result = firstArray.Union(secondArray);
+                Console.Write(result.Count());
         }
     }
 }
