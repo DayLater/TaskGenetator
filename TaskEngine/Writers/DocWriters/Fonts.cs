3648deacfd82df4ac98cbddde39f7584e596d219
Daulet Basharov <besh98@bk.ru>
2021-05-16 21:55:21 +0300

    add: fonts

--- TaskEngine/Writers/DocWriters/Fonts.cs
+++ TaskEngine/Writers/DocWriters/Fonts.cs
@@ -5,10 +5,18 @@ namespace TaskEngine.Writers.DocWriters
     public static class Fonts
     {
         public const string TimesNewRoman = "Times New Roman";
+        public const string Arial = "Arial";
+        public const string Calibri = "Calibri";
+        public const string Tahoma = "Tahoma";
+        public const string Verdana = "Verdana";
         
         public static IEnumerable<string> GetFonts()
         {
             yield return TimesNewRoman;
+            yield return Arial;
+            yield return Calibri;
+            yield return Tahoma;
+            yield return Verdana;
         } 
     }
 }
